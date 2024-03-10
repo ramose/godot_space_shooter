05abea23af098a63e2e3407ac0b184220f895851
Leo <leopapilaya@gmail.com>
2024-03-10 19:34:25 +0800

    Create meteor blueprint

--- level.gd
+++ level.gd
@@ -1,15 +1,9 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var meteor_scene : PackedScene = load("res://scenes/meteor.tscn")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-func _on_timer_timeout():
-	print('meteor') # Replace with function body.
+func _on_meteor_timer_timeout():
+	var meteor = meteor_scene.instantiate()
+	
+	$Meteors.add_child(meteor)
